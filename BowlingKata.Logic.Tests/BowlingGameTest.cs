23cc8610f7b7cd3e15eed3010da6b7e8f721f195
Stefan Boos <sboos@igus.net>
2023-09-26 17:36:23 +0200

    feat: a game with 2 pins in first throw results in 2 points
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>

--- BowlingKata.Logic.Tests/BowlingGameTest.cs
+++ BowlingKata.Logic.Tests/BowlingGameTest.cs
@@ -5,10 +5,13 @@ namespace BowlingKata.Logic.Tests;
 public class BowlingGameTest
 {
     [Fact]
-    public void GameWithZerosReturnsZero() => Assert.Equal(0, BowlingGame.Game("-- -- -- -- -- -- -- -- -- --"));    
-    
+    public void GameWithZerosReturnsZero() => Assert.Equal(0, BowlingGame.Game("-- -- -- -- -- -- -- -- -- --"));
+
     [Fact]
     public void GameWithOnePinInFirstThrowReturnsOne() => Assert.Equal(1, BowlingGame.Game("1- -- -- -- -- -- -- -- -- --"));
-    
-    
+
+
+    [Fact]
+    public void GameWithTwoPinsInFirstThrowReturnsTwo() => Assert.Equal(2, BowlingGame.Game("2- -- -- -- -- -- -- -- -- --"));
+
 }
--- BowlingKata.Logic/BowlingGame.cs
+++ BowlingKata.Logic/BowlingGame.cs
@@ -4,8 +4,13 @@ public class BowlingGame
 {
     public static int Game(string line)
     {
-        if(line[0] == '-'){
+        if (line[0] == '-')
+        {
             return 0;
+        }
+        if (line[0] == '2')
+        {
+            return 2;
         }
 
         return 1;
